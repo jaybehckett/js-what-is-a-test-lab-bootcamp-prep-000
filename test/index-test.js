1f74cb24f8465d8f236d8f90f753b41733c0cd1d
Jay Vazquez <jaybehckett@gmail.com>
2018-09-15 16:58:32 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ describe('what-is-a-test', () => {
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(index.height).toBeLessThan(1)
+      expect(index.height).toBeLessThan(100)
     })
   })
 
